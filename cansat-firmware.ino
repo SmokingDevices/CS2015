08d870b955bc65dddeac2aab122866703a33c42b
Ale-c <alecfdent@gmail.com>
2015-06-20 19:29:14 +0200

    final version

--- cansat-firmware.ino
+++ cansat-firmware.ino
@@ -7,6 +7,11 @@
 #include <SD.h>
 #define beepPin 5 // digitaler D-Pin 5 für Beeper
 #define SERPERATOR ","
+#define tempNTCPin A2 // ext. Sensor NTC (2 Beine)
+// NTC-sensor variables
+#define NTC_Grad 18.1699	// The gradiant value for the NTC sensor
+#define NTC_Off 21.0	// The offset value for the NTC sensor
+int HIH4030_Pin = A8; //analog pin 8
 const int chipSelect = 27;
 #define dustPin A9 // angeschlossen an Pin A9
 #define dustLEDPin 4
@@ -37,13 +42,16 @@ boolean readGPS = false;
 int dustValue = 0; // Wert der vom Dust Sensor kommt
 float voltage = 0; // errechnete Voltzahl
 float dustDensity = 0; // letztendliche Staubkonzentration
-String oldGpsData;
 long lastGpsReadMillies = 0;
+unsigned long  lastFullMillies = 0;
+boolean doBeep = false;
+unsigned long lastBeep = 0;
 
 /**************************************************************************************/
 
 void setup(){
   Serial.begin(19200);
+  Serial1.begin(19200);
   for (int i= 16; i < 24; i++) {
     pinMode(i, OUTPUT);
     digitalWrite(i, HIGH);
@@ -82,6 +90,7 @@ void setup(){
   }
   digitalWrite(20, LOW);
   delay (500);
+
   
   float pressureSum = 0;
   for (int i = 0; i < GroundPressureTestCount; i++) {
@@ -96,7 +105,9 @@ void setup(){
   groundPressure = pressureSum / GroundPressureTestCount;
   Serial.print("GroundPressure=");
   Serial.println(groundPressure);
-
+  
+  Serial.println ("Secs,currentheight,accelx,accely,accelz,magx,magy,magz,gyrox,gyroy,gyroz,pressure,Temp,Dustdensity,Temp.ext,humidity,GPS"); 
+  
   digitalWrite(21, LOW);
   delay (500);
   
@@ -118,6 +129,7 @@ void setup(){
   if (myFile) {
     Serial.print("File opened");
     myFile.println("-----------------------------------SNIP----------------------------------");
+    myFile.println("Secs,currentheight,pressure,Temp,Dustdensity,Temp.ext,humidity,GPS"); 
     // close the file:
     myFile.close();
     Serial.println("done.");
@@ -126,6 +138,21 @@ void setup(){
     Serial.println("File couldnt be opened");
     return;
   }
+  
+  File my10DOFFile = SD.open("10dof.txt", FILE_WRITE);
+  if (my10DOFFile) {
+    Serial.print("File opened");
+    my10DOFFile.println("-----------------------------------SNIP----------------------------------");
+    my10DOFFile.println("accelx,accely,accelz,magx,magy,magz,gyrox,gyroy,gyroz"); 
+    // close the file:
+    my10DOFFile.close();
+    Serial.println("done.");
+  } else {
+    // if the file didn't open, print an error:
+    Serial.println("File couldnt be opened");
+    return;
+  }
+  
   
   Serial.println("Card initialised");
   digitalWrite(23, LOW);
@@ -147,8 +174,60 @@ void setup(){
 
   
 void loop() {
+  read10dof();
   
-  // read out all the sensor values
+  if (doBeep && lastBeep < millis()) {
+      digitalWrite(beepPin, !digitalRead(beepPin));
+      lastBeep = millis() + 200;
+  }
+//  Serial.print((lastFullMillies + 1000));
+//  Serial.print(" ");
+//  Serial.println(millis());
+  if ((lastFullMillies + 1000) < millis()) {
+    // read out all the sensor values
+    sensors_event_t preshureEvent;
+    float currentHeight;
+    bmp.getEvent(&preshureEvent); // get pressure
+    float temperature; // Temp
+    bmp.getTemperature(&temperature); // write temp into float
+    float pressure = preshureEvent.pressure; // write current pressure to float
+    dustDensity = readDustSensor();// read dust value from sensor
+  
+    float externalTemperature = getTemperatureExtern(tempNTCPin);// read NTC
+    float humidity = getHumidity(temperature);// read humiditySensor
+
+    // calculate the current height
+    currentHeight = bmp.pressureToAltitude(groundPressure,preshureEvent.pressure); // calculating the altitude from pressure
+    if (!wasAbove50m && currentHeight > 50) {
+      wasAbove50m = true;   
+    }
+    // check if passed the boarder and switch on beeping if passed
+    if (wasAbove50m && currentHeight < 50) {
+      doBeep = true;
+    }
+  
+    // read out gps data
+    String gpsString = "";
+    if ((millis() - lastGpsReadMillies) > 1000) {
+      String temp = "";
+      if (!readGPS) {
+        temp = GetGGA();
+        lastGpsReadMillies = millis();
+        if (!temp.equals("")) {
+          gpsString += temp;
+        }
+      }
+    }
+    // save all the values
+    saveData(currentHeight, preshureEvent.pressure, temperature, dustDensity, externalTemperature, humidity, gpsString);
+    lastFullMillies = millis();
+  }
+  delay(100);
+
+}
+
+void read10dof () {
+// read out all the sensor values
   sensors_event_t accEvent; 
   accel.getEvent(&accEvent);// get acceleration data
   sensors_event_t magEvent;
@@ -156,46 +235,37 @@ void loop() {
   sensors_event_t gyroEvent;
   gyro.getEvent(&gyroEvent); // get gyrocope values
   sensors_event_t preshureEvent;
-  float currentHeight;
-  bmp.getEvent(&preshureEvent); // get pressure
-  float temperature; // Temp
-  bmp.getTemperature(&temperature); // write temp into float
-  float pressure = preshureEvent.pressure; // write current pressure to float
-  dustDensity = readDustSensor();// read dust value from sensor
-  
-  float externalTemperature = 99;// read NTC
-  float humidity = 111;// read humiditySensor
 
-  // calculate the current height
-  currentHeight = bmp.pressureToAltitude(groundPressure,preshureEvent.pressure); // calculating the altitude from pressure
-  if (!wasAbove50m && currentHeight > 50) {
-    wasAbove50m = true;   
-  }
-  // check if passed the boarder and switch on beeping if passed
-  if (wasAbove50m && currentHeight < 50) {
-    digitalWrite(beepPin, HIGH);
-    delay (200);
-    digitalWrite(beepPin, LOW);  
+
+  File my10DOFFile = SD.open("10dof.txt", FILE_WRITE);
+  if (my10DOFFile) {
+    my10DOFFile.print(SERPERATOR);
+    my10DOFFile.print(accEvent.acceleration.x);
+    my10DOFFile.print(SERPERATOR);
+    my10DOFFile.print(accEvent.acceleration.y);
+    my10DOFFile.print(SERPERATOR);
+    my10DOFFile.print(accEvent.acceleration.z);
+    my10DOFFile.print(SERPERATOR);
+    my10DOFFile.print(magEvent.magnetic.x);
+    my10DOFFile.print(SERPERATOR);
+    my10DOFFile.print(magEvent.magnetic.y);
+    my10DOFFile.print(SERPERATOR);
+    my10DOFFile.print(magEvent.magnetic.z);
+    my10DOFFile.print(SERPERATOR);
+    my10DOFFile.print(gyroEvent.gyro.x);
+    my10DOFFile.print(SERPERATOR);
+    my10DOFFile.print(gyroEvent.gyro.y);
+    my10DOFFile.print(SERPERATOR);
+    my10DOFFile.print(gyroEvent.gyro.z);
+    my10DOFFile.println();
+    my10DOFFile.close();
+  } else {
+    // if the file didn't open, print an error:
+    Serial.println("10DOF file couldn't be opened");
+    return;
   }
   
-  // read out gps data
-  if ((millis() - lastGpsReadMillies) > 1000) {
-    String gpsString = "";
-    String temp = "";
-    if (!readGPS) {
-      temp = GetGGA();
-      lastGpsReadMillies = millis();
-      if (!temp.equals("")) {
-        gpsString += temp;
-        oldGpsData = gpsString;
-      }
-    }
-  }
-  
-  // save all the values
-  saveData(currentHeight, accEvent.acceleration.x, accEvent.acceleration.y, accEvent.acceleration.z, magEvent.magnetic.x, magEvent.magnetic.y, magEvent.magnetic.z,
-           gyroEvent.gyro.x, gyroEvent.gyro.y, gyroEvent.gyro.z, preshureEvent.pressure, temperature, dustDensity, externalTemperature, humidity, oldGpsData);
-  delay(100);
+
 
 }
 
@@ -249,34 +319,15 @@ String GetGGA() {
 }
 
 
-void saveData(float currentHeight, float accelX, float accelY, float accelZ, float magX, float magY, float magZ, float gyroX, float gyroY, float gyroZ,
-              float pressure, float temperature, float dustDensity, float externalTemp, float humidity, String gpsString) {
+void saveData(float currentHeight, float pressure, float temperature, float dustDensity, float externalTemp, float humidity, String gpsString) {
 
   // open file on SD card              
   File myFile = SD.open("data.txt", FILE_WRITE);
               
   // print all the data to each channel
-  printData(millis(), myFile);
+  printData(millis()/1000, myFile);
   printData(SERPERATOR, myFile);
   printData(currentHeight, myFile);
-  printData(SERPERATOR, myFile);
-  printData(accelX, myFile);
-  printData(SERPERATOR, myFile);
-  printData(accelY, myFile);
-  printData(SERPERATOR, myFile);
-  printData(accelZ, myFile);
-  printData(SERPERATOR, myFile);
-  printData(magX, myFile);
-  printData(SERPERATOR, myFile);
-  printData(magY, myFile);
-  printData(SERPERATOR, myFile);
-  printData(magZ, myFile);
-  printData(SERPERATOR, myFile);
-  printData(gyroX, myFile);
-  printData(SERPERATOR, myFile);
-  printData(gyroY, myFile);
-  printData(SERPERATOR, myFile);
-  printData(gyroZ, myFile);
   printData(SERPERATOR, myFile);
   printData(pressure, myFile);
   printData(SERPERATOR, myFile);
@@ -326,3 +377,34 @@ void endLine (File myFile) {
     myFile.println();
   }
 }
+
+float getTemperatureExtern(int pin){
+  float volt = Bit2Volt(pin); // liest pin ein
+  float Temp=NTC_Grad*volt-NTC_Off; // umrechnen Volt nach Grad Celcius
+  return Temp; // zurueckgeben
+}
+
+float Bit2Volt(int n){ // Funktion zum Lesen und umwandeln
+  int raw = analogRead(n); // Messdaten von pin 'n' lesen
+  float volt = (float)raw*5.000/1023; // Umrechnung für Volt
+  return volt; // zurückgeben der Voltzahl
+}
+
+float getHumidity(float degreesCelsius){
+  //caculate relative humidity
+  float supplyVolt = 5.0;
+
+  // read the value from the sensor:
+  int HIH4030_Value = analogRead(HIH4030_Pin);
+  float voltage = HIH4030_Value/1023. * supplyVolt; // convert to voltage value
+  //voltage = 1.8;
+  // convert the voltage to a relative humidity
+  // - the equation is derived from the HIH-4030/31 datasheet
+  // - it is not calibrated to your individual sensor
+  //  Table 2 of the sheet shows the may deviate from this line
+  float sensorRH = 161.0 * voltage / supplyVolt - 25.8;
+  float trueRH = sensorRH / (1.0546 - 0.0026 * degreesCelsius); //temperature adjustment 
+  return trueRH;
+}
+
+
